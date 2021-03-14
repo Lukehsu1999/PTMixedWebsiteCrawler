ccc736c9ec2473fd24a079798193d169ba45dd33
Alwin <alwin60805@gmail.com>
2021-03-13 20:22:32 -0500

    [add] venv

--- build/lib/pt_mixed_website_crawler/spiders/test.py
+++ build/lib/pt_mixed_website_crawler/spiders/test.py
@@ -1,9 +1,11 @@
 import scrapy
-
 class NewsSpider(scrapy.Spider):
     name = 'newspider'
     start_urls = ['https://newbloommag.net/category/main/politics/']
 
     def parse(self, response):
-        for title in response.css('.cb-post-title'):
-            yield {'title': title.css('::text').get()}
+        for post in response.css('article'):
+            yield {
+                'title': post.css('.cb-post-title a::text')[0].get(),
+                'content': post.css('.cb-excerpt ::text')[0].get()
+            }
