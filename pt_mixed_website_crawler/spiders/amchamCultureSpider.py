ac619949ef8a32a9b3609fe9755a933d172b2349
Alwin <alwin60805@gmail.com>
2021-07-04 13:04:40 +0800

    [add] truncate blurb into 163 characters

--- pt_mixed_website_crawler/spiders/amchamCultureSpider.py
+++ pt_mixed_website_crawler/spiders/amchamCultureSpider.py
@@ -3,11 +3,17 @@ class AmchamCultureSpider(scrapy.Spider):
     name = 'amchamCultureSpider'
     start_urls = ['https://topics.amcham.com.tw/category/wine-dine/', 'https://topics.amcham.com.tw/category/travel-and-culture/']
 
+    def smart_truncate(self, content, length=163, suffix='...'):
+        if len(content) <= length:
+            return content
+        else:
+            return ' '.join(content[:length+1].split(' ')[0:-1]) + suffix
+
     def parse(self, response):
         for post in response.css('article'):
             yield {
                 'Topic': post.css('.entry-title a::text')[0].get(),
-                'Blurb': post.css('.entry-summary p::text')[0].get(),
+                'Blurb': self.smart_truncate(post.css('.entry-summary p::text')[0].get()),
                 'Image': post.css('img::attr(src)').extract()[0],
                 'Link': post.css('.entry-title a::attr(href)').extract()[0],
                 'OP': "Taiwan Business Topics"
