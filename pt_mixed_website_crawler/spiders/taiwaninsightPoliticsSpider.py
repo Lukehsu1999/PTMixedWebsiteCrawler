a17886358eeb23ccc9e3fe6d56520ee48a9c1500
Alwin <alwin60805@gmail.com>
2021-07-04 12:32:31 +0800

    [fix] image display and title

--- pt_mixed_website_crawler/spiders/taiwaninsightPoliticsSpider.py
+++ pt_mixed_website_crawler/spiders/taiwaninsightPoliticsSpider.py
@@ -8,7 +8,7 @@ class TaiwaninsightPoliticsSpider(scrapy.Spider):
             yield {
                 'Topic': post.css('.entry-title a::text')[0].get(),
                 'Blurb': post.css('.entry-content p::text')[0].get(),
-                'Image': "https://static.wixstatic.com/media/3221e1_6f6d781619524c42b54b0b3a746f80fe~mv2.jpeg",
+                'Image': post.css('img::attr(src)').extract()[0],
                 'Link': post.css('.entry-title a::attr(href)').extract()[0],
-                'OP': "Taiwanese American.org"
+                'OP': "Taiwanese Insight"
             }
